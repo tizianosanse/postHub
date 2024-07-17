77cd760ca4d8194d3c3b5eee506551a1ca6193ba
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-07-17 16:05:48 +0200

    add project

---
+++ PostHub/src/main/java/tizianosanseverino/PostHub/PostHubApplication.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.PostHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostHubApplication.class, args);
+	}
+
+}
---
+++ PostHub/src/test/java/tizianosanseverino/PostHub/PostHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.PostHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
